5fc49eb8d7171462faf83f5bf34c5d4d706b7397
Hasan Melih Akbulut <57303760+Melihakbulut221@users.noreply.github.com>
2024-03-14 19:27:45 +0300

    Create config_requirements.py

---
+++ config_requirements.py
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y python3-venv
+
+mkdir object_detection_ele495
+cd object_detection_ele495/
+~/object_detection_ele495#
+
+~/object_detection_ele495# python3 -m venv object_venv
+
+~/object_detection_ele495# source object_venv/bin/activate
+
+~/object_detection_ele495# pip install -U setuptools pip
+
+sudo apt install python3.12
+
